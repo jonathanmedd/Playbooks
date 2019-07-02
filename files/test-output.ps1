2bf70515df573e2c04f1e5c4552aa5d43daaa754
Jonathan Medd <jonathan.medd.external@atos.net>
2019-07-02 13:17:05 +0100

    test

--- files/test-output.ps1
+++ files/test-output.ps1
@@ -1,3 +1,6 @@
 Write-Host "And you, young Skywalker. We will watch your career with great interest."
 
 Write-Host "Always two, there are. No more, no less. A master and an apprentice"
+
+Write-Host "VM: ad101"
+Write-Host "VM: dockerhost-02"
