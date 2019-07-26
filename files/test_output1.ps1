aba0c17fc75700a6e648e9d6d158136e171b9d7e
Jonathan Medd <jonathan.medd.external@atos.net>
2019-07-26 16:25:52 +0100

    test

---
+++ files/test_output1.ps1
@@ -0,0 +1,6 @@
+Write-Host "And you, young Skywalker. We will watch your career with great interest."
+
+Write-Host "Always two, there are. No more, no less. A master and an apprentice"
+
+Write-Host "VM: 10.0.3.4"
+Write-Host "VM: 10.0.3.6"
